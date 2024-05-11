24ecd4dc2b997f1c4e4c32b54a9fe1017a750581
Majdi Aribi <majdi.developer@gmail.com>
2024-05-11 09:29:35 +0100

    Common button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:casting_app/core/theme/theme.dart';
+import 'package:casting_app/core/widgets/common_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -90,21 +92,14 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            CommonButton(
+              text: 'connexion',
+              background: roseColor,
+              colorText: whiteColor,
+              onPressed: () {},
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
